9cdc868880aca2b265ece083fb9b53413f0a553c
julielpb <julie.blanquefort@outlook.com>
2016-11-30 17:40:33 +0100

    version 1.1.0

--- src/main/java/geco/PasswordGeneration.java
+++ src/main/java/geco/PasswordGeneration.java
@@ -4,4 +4,20 @@ package geco;
  * Created by 21301266 on 30/11/2016.
  */
 public class PasswordGeneration {
+    String mdp;
+
+    public PasswordGeneration() {
+
+    }
+
+    public String getRandomPassword() {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String pass = "";
+        for(int x=0;x<8;x++)
+        {
+            int i = (int)Math.floor(Math.random() * 62);
+            pass += chars.charAt(i);
+        }
+        return pass;
+    }
 }
--- src/test/java/geco/TestPasswordGenerator.java
+++ src/test/java/geco/TestPasswordGenerator.java
@@ -1,7 +1,24 @@
 package geco;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by 21301266 on 30/11/2016.
  */
 public class TestPasswordGenerator {
+    PasswordGeneration log;
+
+    @Before
+    public void setUp(){
+        log = new PasswordGeneration();
+    }
+
+    @Test
+    public void testLoginExists(){
+        assertEquals(8,log.getRandomPassword().length());
+        assertNotEquals(log.getRandomPassword(), log.getRandomPassword());
+    }
 }
